df2b7998f69aa5e3ea45de8719c9ea74304ddebd
muhammadfadlan31651-collab <muhammad.fadlan31651@sma.belajar.id>
2026-04-16 11:28:41 +0700

    Update soal5.cpp

--- soal5.cpp
+++ soal5.cpp
@@ -1,1 +1,31 @@
+// buat program yang menerima sebuah bilangan bulat dari pengguna, lalu tentukan apakah bilangan tersebut prima atau bukan prima.
+// contoh:
+// input: 5 ; output : prima
+// input: 78 ; output: bukan prima
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "input angka: ";
+    cin >> n;      
+    bool prima = true; // anggap dulu bilangan prima
+
+    if (n <= 1) prima = false; // bilangan <= 1 bukan prima
+    else {
+        for (int i = 2; i * i <= n; i++) {   // cek pembagi dari 2 sampai akar n
+            if (n % i == 0) {                // jika ada pembagi habis
+                prima = false;               // berarti bukan prima
+                break;                       // hentikan loop
+            }
+        }
+    }
+
+    if (prima) cout << "Prima" << endl;
+    else cout << "Bukan Prima" << endl;
+
+    return 0;
+}
+    
 
