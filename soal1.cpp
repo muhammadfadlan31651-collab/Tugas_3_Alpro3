d803dc637c35d6c8b5a84e099b47e387f4101722
mei <meisrikw@gmail.com>
2026-04-16 21:23:20 +0700

    Update soal1.cpp

--- soal1.cpp
+++ soal1.cpp
@@ -7,32 +7,20 @@ contoh input            contoh output
 
 #include <iostream>
 using namespace std;
+
 int main() {
-    int n;      
-    int nilai;   
+    int n;       // variabel untuk menyimpan jumlah suku yang ingin ditampilkan
+    int nilai;   // variabel untuk menyimpan nilai suku saat ini
+
     cout << "Masukkan jumlah suku: ";
     cin >> n;
     nilai = 1;   // suku pertama selalu dimulai dari 1
 
-    for (int i = 1; i <= n; i++) {
-        /*
-        Cetak nilai suku ke-i.
-        Jika belum suku terakhir, cetak spasi setelahnya.
-        */
-        cout << nilai;
+    for (int i = 1; i <= n; i++) {    // jika i ganjil (1,3,5,....) => tambah 5
+        cout << nilai;                // jika i genap (2,4,6,....) => tambah 5
         if (i < n) cout << " ";
 
-        /*
-        LOGIKA POLA:
-        - Jika i ganjil (1, 3, 5, ...) => tambah 5
-        - Jika i genap (2, 4, 6, ...) => tambah 3
-        Contoh:
-            i=1 (ganjil) => 1 + 5 = 6
-            i=2 (genap)  => 6 + 3 = 9
-            i=3 (ganjil) => 9 + 5 = 14
-            i=4 (genap)  => 14+ 3 = 17
-            dst...
-        */
+        
         if (i % 2 == 1) {
             nilai += 5;   // i % 2 == 1 artinya i adalah bilangan ganjil
         } else {
